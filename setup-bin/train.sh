70f9955b1dcaa6eafe9fc0f1cf045ae5bdb95a55
Aaron Madlon-Kay <aaron@madlon-kay.com>
2016-05-18 16:20:19 +0900

    Truecase input files

--- setup-bin/train.sh
+++ setup-bin/train.sh
@@ -3,34 +3,46 @@
 mkdir -p ${WORK_HOME}/corpus
 cd ${WORK_HOME}/corpus
 
+# Truecase
+for LANG in $SOURCE_LANG $TARGET_LANG; do
+    ${MOSES_HOME}/scripts/recaser/train-truecaser.perl \
+                 --model truecase-model.${LANG} \
+                 --corpus ${DATA_HOME}/train/bitext.tok.${LANG}
+    ${MOSES_HOME}/scripts/recaser/truecase.perl \
+                 --model truecase-model.${LANG} \
+                 < ${DATA_HOME}/train/bitext.tok.${LANG} \
+                 > bitext.true.${LANG}
+done
+
 # Clean
 ${MOSES_HOME}/scripts/training/clean-corpus-n.perl \
-	${DATA_HOME}/train/bitext.tok ${TARGET_LANG} ${SOURCE_LANG} \
-	${WORK_HOME}/corpus/bitext.clean 1 80
+	bitext.true ${TARGET_LANG} ${SOURCE_LANG} \
+	bitext.clean 1 80
 
 # Make language model
 mkdir ${WORK_HOME}/lm
 cd ${WORK_HOME}/lm
 ${MOSES_HOME}/bin/lmplz -o 3 ${TEST_MODE:+--discount_fallback} \
              < ${WORK_HOME}/corpus/bitext.clean.${TARGET_LANG} \
-             > ${WORK_HOME}/lm/bitext.arpa.${TARGET_LANG}
+             > bitext.arpa.${TARGET_LANG}
 ${MOSES_HOME}/bin/build_binary \
-	${WORK_HOME}/lm/bitext.arpa.${TARGET_LANG} \
-	${WORK_HOME}/lm/bitext.blm.${TARGET_LANG}
+	bitext.arpa.${TARGET_LANG} \
+	bitext.blm.${TARGET_LANG}
 
 # Train
 mkdir ${WORK_HOME}/train
 cd ${WORK_HOME}/train
 ${MOSES_HOME}/scripts/training/train-model.perl -root-dir ${WORK_HOME}/train \
 	-corpus ${WORK_HOME}/corpus/bitext.clean \
-	-f ${SOURCE_LANG} -e ${TARGET_LANG} -alignment grow-diag-final-and -reordering msd-bidirectional-fe \
+	-f ${SOURCE_LANG} -e ${TARGET_LANG} \
+    -alignment grow-diag-final-and -reordering msd-bidirectional-fe \
 	-lm 0:3:${WORK_HOME}/lm/bitext.blm.${TARGET_LANG}:8 \
-	-external-bin-dir ${BIN_HOME}/tools -cores $(nproc) > ${WORK_HOME}/train/training.out
+	-external-bin-dir ${BIN_HOME}/tools -cores $(nproc) > training.out
 
 if [ "${TARGET_LANG}" = "ja" ] || [ "${TARGET_LANG}" = "zh" ] || \
 	[ "${SOURCE_LANG}" = "ja" ] || [ "${SOURCE_LANG}" = "zh" ]; then
 	echo "For ja or zh: Changing [distortion-limit] to -1 in train/model/moses.ini"
-	INI=${WORK_HOME}/train/model/moses.ini
+	INI=model/moses.ini
 	sed -r '/^\[distortion-limit\]$/{n;s/\S+/-1/}' < ${INI} > ${INI}-temp
-	mv ${INI}-temp ${INI}
+	mv ${INI}{-temp,}
 fi
--- setup-bin/tune.sh
+++ setup-bin/tune.sh
@@ -3,10 +3,27 @@
 mkdir -p ${WORK_HOME}/tune
 cd ${WORK_HOME}/tune
 
+# Truecase
+for LANG in $SOURCE_LANG $TARGET_LANG; do
+    ${MOSES_HOME}/scripts/recaser/train-truecaser.perl \
+                 --model truecase-model.${LANG} \
+                 --corpus ${DATA_HOME}/tune/bitext.tok.${LANG}
+    ${MOSES_HOME}/scripts/recaser/truecase.perl \
+                 --model truecase-model.${LANG} \
+                 < ${DATA_HOME}/tune/bitext.tok.${LANG} \
+                 > bitext.true.${LANG}
+done
+
+# Clean
+${MOSES_HOME}/scripts/training/clean-corpus-n.perl \
+	bitext.true ${TARGET_LANG} ${SOURCE_LANG} \
+	bitext.clean 1 80
+
+
 # Tune
 ${MOSES_HOME}/scripts/training/mert-moses.pl \
-	${DATA_HOME}/tune/bitext.tok.${SOURCE_LANG} \
-    ${DATA_HOME}/tune/bitext.tok.${TARGET_LANG} \
+	bitext.clean.${SOURCE_LANG} \
+    bitext.clean.${TARGET_LANG} \
 	${MOSES_HOME}/bin/moses ${WORK_HOME}/train/model/moses.ini \
     --mertdir ${MOSES_HOME}/bin/ \
-	--decoder-flags="-threads $(nproc)" > ${WORK_HOME}/tune/mert.out
+	--decoder-flags="-threads $(nproc)" > mert.out
