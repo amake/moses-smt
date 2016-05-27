091ddd64228b2df72bbd6cf49545d447550f8713
Aaron Madlon-Kay <aaron@madlon-kay.com>
2016-05-27 21:51:55 +0900

    Improve REPL

--- mosesxmlrpcrepl.py
+++ mosesxmlrpcrepl.py
@@ -1,22 +1,52 @@
 #!/usr/bin/env python
 
+import re
 import sys
 import xmlrpclib
+import tinysegmenter
 
 
-def repl(server):
-    try:
-        while True:
-            query = raw_input('Query: ')
-            response = server.translate({'text': query})
-            print response['text']
-    except KeyboardInterrupt:
-        print('Exiting')
+class XmlRpcRepl(object):
+    def __init__(self, url):
+        self.url = url
+        self.server = xmlrpclib.ServerProxy(url)
+
+    def go(self):
+        try:
+            while True:
+                print(self.evaluate(self.read()))
+        except KeyboardInterrupt:
+            print('Exiting')
+
+    def read(self):
+        return raw_input('Query: ').decode('utf-8')
+
+    def evaluate(self, inpt):
+        raise NotImplementedError
+
+
+class MosesRepl(XmlRpcRepl):
+    def __init__(self, *args, **kwargs):
+        super(MosesRepl, self).__init__(*args, **kwargs)
+        self.ts = tinysegmenter.TinySegmenter()
+
+    def evaluate(self, inpt):
+        return self.do_query(self.clean_input(inpt))
+
+    def clean_input(self, text):
+        return ' '.join(self.ts.tokenize(text))
+
+    def do_query(self, query):
+        response = self.server.translate({'text': query})
+        return self.clean_response(response['text'])
+
+    def clean_response(self, text):
+        return re.sub(ur'(?<=[\u3001-\u9fa0])\s+(?=[\u3001-\u9fa0])', '', text)
 
 
 def main():
-    server = xmlrpclib.ServerProxy(sys.argv[1])
-    repl(server)
+    url = sys.argv[1] if len(sys.argv) > 1 else raw_input('URL: ')
+    MosesRepl(url).go()
 
 
 if __name__ == '__main__':
