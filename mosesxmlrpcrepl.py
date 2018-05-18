0e1027dcf581ce51bd8cae7a1a89f74b406542f4
Aaron Madlon-Kay <aaron@madlon-kay.com>
2018-05-18 20:26:19 +0900

    Add --no-clean flag to mosesxmlrpcrepl

--- mosesxmlrpcrepl.py
+++ mosesxmlrpcrepl.py
@@ -6,12 +6,16 @@ import re
 import sys
 import xmlrpclib
 import tinysegmenter
+from HTMLParser import HTMLParser
+
+html_parser = HTMLParser()
 
 
 class XmlRpcRepl(object):
-    def __init__(self, url, raw=False):
+    def __init__(self, url, raw=False, clean=True):
         self.url = url
         self.raw = raw
+        self.clean = clean
         self.server = xmlrpclib.ServerProxy(url)
 
     def go(self):
@@ -36,8 +40,10 @@ class MosesRepl(XmlRpcRepl):
     def evaluate(self, inpt):
         query = inpt
         if not self.raw:
-            query = self.clean_input(inpt)
-            logging.debug("Cleaned input: %s", query)
+            query = query.decode('string_escape')
+            if self.clean:
+                query = self.clean_input(inpt)
+                logging.debug("Cleaned input: %s", query)
         return self.do_query(query)
 
     def clean_input(self, text):
@@ -47,8 +53,10 @@ class MosesRepl(XmlRpcRepl):
         response = self.server.translate({'text': query})
         response_text = response['text']
         if not self.raw:
-            logging.debug("Raw response: %s", response)
-            response_text = self.clean_response(response_text)
+            response_text = html_parser.unescape(response_text)
+            if self.clean:
+                logging.debug("Raw response: %s", response_text)
+                response_text = self.clean_response(response_text)
         return response_text
 
     def clean_response(self, text):
@@ -60,6 +68,7 @@ def main():
         description='Interactively query a Moses server')
     parser.add_argument('--verbose', '-v', action='count', default=0)
     parser.add_argument('--raw', action='store_true')
+    parser.add_argument('--no-clean', action='store_false', dest='clean')
     parser.add_argument('url', nargs='?')
     args = parser.parse_args()
 
@@ -68,7 +77,7 @@ def main():
     logging.basicConfig(level=level)
 
     url = args.url or raw_input('URL: ')
-    MosesRepl(url, raw=args.raw).go()
+    MosesRepl(url, raw=args.raw, clean=args.clean).go()
 
 
 if __name__ == '__main__':
