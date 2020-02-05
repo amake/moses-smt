56aa2558e7307c5c948690bc3b1fa39e5041c8e2
Aaron Madlon-Kay <aaron@wovn.io>
2020-02-05 10:49:03 +0900

    shfmt

--- test.sh
+++ test.sh
@@ -7,8 +7,8 @@ WORK_DIR=work/$LABEL-$SOURCE_LANG-$TARGET_LANG
 
 for CORPUS in train tune; do
     mkdir -p $WORK_DIR/corpus-$CORPUS
-    echo "foo bar baz" > $WORK_DIR/corpus-$CORPUS/bitext.tok.$SOURCE_LANG
-    echo "ほげ ふが ぴよ" > $WORK_DIR/corpus-$CORPUS/bitext.tok.$TARGET_LANG
+    echo "foo bar baz" >$WORK_DIR/corpus-$CORPUS/bitext.tok.$SOURCE_LANG
+    echo "ほげ ふが ぴよ" >$WORK_DIR/corpus-$CORPUS/bitext.tok.$TARGET_LANG
 done
 
 make build run TEST_MODE=true
